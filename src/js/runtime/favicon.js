285e527490dea01261ed7a0b6df1214b3f604b39
Ronak Chakraborty <ronakchak2569@gmail.com>
2026-07-10 17:24:14 -0700

    Share favicon DOM observation

--- src/js/runtime/favicon.js
+++ src/js/runtime/favicon.js
@@ -1,4 +1,5 @@
 import browser from 'webextension-polyfill'
+import { subscribeDomChanges } from './domObserver.js'
 
 const FAVICON_ATTR = 'data-gpth-favicon'
 
@@ -8,54 +9,47 @@ function resolveExtensionUrl(assetUrl) {
 	return browser.runtime.getURL(assetUrl.replace(/^\//, ''))
 }
 
+function recordTouchesHead(record) {
+	if (document.head?.contains(record.target)) return true
+	for (const node of record.addedNodes) {
+		if (node === document.head) return true
+	}
+	return false
+}
+
 function mountFavicon(assetUrl) {
 	const resolvedUrl = resolveExtensionUrl(assetUrl)
 	if (!resolvedUrl) return () => {}
 
 	let favicon = null
-	let headObserver = null
-	let documentObserver = null
 	let applying = false
 	let disposed = false
 
 	function apply() {
 		if (disposed || applying || !document.head) return
 		applying = true
-
-		favicon = document.head.querySelector(`link[${FAVICON_ATTR}]`) || favicon
-		if (!favicon) {
-			favicon = document.createElement('link')
-			favicon.rel = 'icon'
-			favicon.setAttribute(FAVICON_ATTR, '')
+		try {
+			favicon = document.head.querySelector(`link[${FAVICON_ATTR}]`) || favicon
+			if (!favicon) {
+				favicon = document.createElement('link')
+				favicon.rel = 'icon'
+				favicon.setAttribute(FAVICON_ATTR, '')
+			}
+			if (favicon.href !== resolvedUrl) favicon.href = resolvedUrl
+			if (document.head.lastElementChild !== favicon) document.head.appendChild(favicon)
+		} finally {
+			applying = false
 		}
-		if (favicon.href !== resolvedUrl) favicon.href = resolvedUrl
-		if (document.head.lastElementChild !== favicon) document.head.appendChild(favicon)
-
-		applying = false
 	}
 
-	function observeHead() {
-		if (disposed || headObserver || !document.head) return
-		documentObserver?.disconnect()
-		documentObserver = null
-		apply()
-		headObserver = new MutationObserver(apply)
-		headObserver.observe(document.head, { childList: true })
-	}
-
-	if (document.head) {
-		observeHead()
-	} else if (document.documentElement) {
-		documentObserver = new MutationObserver(observeHead)
-		documentObserver.observe(document.documentElement, { childList: true })
-	}
+	apply()
+	const unsubscribe = subscribeDomChanges((records) => {
+		if (records.some(recordTouchesHead)) apply()
+	})
 
 	return () => {
 		disposed = true
-		headObserver?.disconnect()
-		documentObserver?.disconnect()
-		headObserver = null
-		documentObserver = null
+		unsubscribe()
 		favicon?.remove()
 		favicon = null
 	}
